102babd9fe020b47e5ff1dcfb9efebb90cdb87b6
Jill Carr <j-carr@kellogg.northwestern.edu>
2016-02-07 13:04:55 -0600

    spotify & page updates

---
+++ books.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(".btn").on("click", addItem);
+    $(".link-delete").on("click", deleteItem);
+    $("ol").on("click", ".link-delete", deleteItem);
+});
+
+function addItem() {
+  // console.log("Yes");
+  var text = window.prompt("Add a Book!");
+  var delete_link = '<a href="#" class="link-delete">(Delete)</a>'
+  $("ol").append("<li>" + text + " " + delete_link + "</li>");
+  //
+  var numItems = $("li").length;
+  $(".total").html(numItems + " items");
+  }
+  if (numItems == 1) {
+      $(".total").html(numItems + " item");
+    }
+    else {
+      $(".total").html(numItems + " items");
+    }
+
+function deleteItem(event) {
+  console.info(event);
+  // $(event.target).remove();
+  $(event.target).parent().remove();
+}
