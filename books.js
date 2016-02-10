fc05409b570c6ee8f5fdb2960e852775c5d17c55
Jill Carr <j-carr@kellogg.northwestern.edu>
2016-02-09 20:05:13 -0600

    images & js

--- books.js
+++ books.js
@@ -10,10 +10,10 @@ function addItem() {
   var delete_link = '<a href="#" class="link-delete">(Delete)</a>'
   $("ol").append("<li>" + text + " " + delete_link + "</li>");
 
-  var numItems = $("li").length;
+  var numItems = $("total").length;
   $(".total").html(numItems + " items");
   }
-  if (numItems == 1) {
+  if (numItems == 1) {   // grr! This now doesn't work! Hate to be grammatically incorrect //
       $(".total").html(numItems + " item");
     }
     else {
@@ -25,3 +25,21 @@ function deleteItem(event) {
   // $(event.target).remove();
   $(event.target).parent().remove();
 }
+
+// function rE(element) // resizeElement
+//     {
+//         $(element).width(0);
+//         $(element).css('width', ($(document).width() + "px"));
+//     }
+//
+// $(window).resize(function() { rE("#footer");  rE("#header"); }); // resizing window
+//
+// rE("#footer");  rE("#header");  // onload the page
+
+// function rF() // resizeFooter
+//     {
+//         $("#footer").css('width', ($(document).width() + "px"));
+//     }
+//
+// $(window).resize(function() {  rF(); }); // resizing window
+// rF(); // onload the page
